8fb2260339b9ccb3afa03f5af87fbc6e8b975536
ICEPINK <venayoda@gmail.com>
2024-05-03 19:34:02 +0200

    vim input update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,4 +3,4 @@ local run_args = ''
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Custom -> diagnostic.open_float' })
 vim.keymap.set('n', '<esc>', vim.cmd.nohlsearch, { desc = 'Custom -> nohlsearch' })
 vim.keymap.set('n', '<f5>', function() vim.cmd('!lua .nvim/run.lua ' .. run_args) end, { desc = 'Custom -> nohlsearch' })
-vim.keymap.set('n', '<f6>', function() run_args = vim.fn.input("run.lua args: ") end, { desc = 'Custom -> run args' })
+vim.keymap.set('n', '<f6>', function() vim.ui.input({ prompt = 'run.lua args: ', default = run_args }, function(input) run_args = input end) end, { desc = 'Custom -> run args' })
