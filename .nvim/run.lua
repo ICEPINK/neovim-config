9765fbc1a07ac4dd7ae4b6cb508770ea0bb5d4ed
ICEPINK <venayoda@gmail.com>
2024-05-03 15:59:34 +0200

    default run script

---
+++ .nvim/run.lua
@@ -0,0 +1,69 @@
+-- .nvim/run.lua ---------------------------------------------------------------
+-- Globals ---------------------------------------------------------------------
+local g_build_type = 'Release'
+local g_executable = '.\\build\\VIS.exe'
+local g_executable_args = {}
+-- FncDefinitions --------------------------------------------------------------
+function in_args(a)
+    for i,v in ipairs(arg) do
+        if v == a then 
+            return true
+        end
+    end
+    return false
+end
+function to_cmd(arr)
+    local result_cmd = ''
+    for i,v in ipairs(arr) do
+        result_cmd = result_cmd .. v .. ' '
+    end
+    return result_cmd
+end
+function setup()
+    print('@setup(cmake)')
+    setup_cmd = {
+        'cmake',
+        '-G "Ninja"',
+        '-S ./',
+        '-B ./build/',
+        '-DCMAKE_BUILD_TYPE=' .. g_build_type,
+    }
+    os.execute(to_cmd(setup_cmd))
+end
+function build()
+    print('@build(cmake)')
+    build_cmd = {
+        'cmake',
+        '--build ./build/',
+    }
+    os.execute(to_cmd(build_cmd))
+end
+function run()
+    print('@run(VIS.exe)')
+    run_cmd = g_executable_args
+    os.execute(g_executable .. to_cmd(run_cmd))
+end
+-- Handle Args -----------------------------------------------------------------
+print('')
+if arg[1] == nil then
+    setup()
+    build()
+    run()
+    return
+end
+if in_args('--debug') then
+    g_build_type = 'Debug'
+end
+if in_args('--release') then
+    g_build_type = 'Release'
+end
+if in_args('--build') then
+    setup()
+    build()
+    return
+end
+if in_args('--setup') then
+    setup()
+    return
+end
+-- End -------------------------------------------------------------------------
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,6 @@
+local run_args = ''
+
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Custom -> diagnostic.open_float' })
 vim.keymap.set('n', '<esc>', vim.cmd.nohlsearch, { desc = 'Custom -> nohlsearch' })
+vim.keymap.set('n', '<f5>', function() vim.cmd('!lua .nvim/run.lua ' .. run_args) end, { desc = 'Custom -> nohlsearch' })
+vim.keymap.set('n', '<f6>', function() run_args = vim.fn.input("run.lua args: ") end, { desc = 'Custom -> run args' })
