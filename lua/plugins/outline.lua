71c2ab715f7b4bbbdbef22df4fc96066aba53b78
ICEPINK <venayoda@gmail.com>
2024-05-04 13:24:07 +0200

    outline plugin

---
+++ lua/plugins/outline.lua
@@ -0,0 +1,11 @@
+return {
+  "hedyhli/outline.nvim",
+  lazy = true,
+  cmd = { "Outline", "OutlineOpen" },
+  keys = {
+    { "<leader>o", "<cmd>Outline<CR>", desc = "Outline -> toggle" },
+  },
+  opts = {
+    -- Your setup opts here
+  },
+}
