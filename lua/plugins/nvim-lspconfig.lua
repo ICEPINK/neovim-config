7e5c26b82a3352a30aeb0dbad735e3342afffcd0
ICEPINK <venayoda@gmail.com>
2024-05-04 13:01:54 +0200

    lsp and definition keymaps added

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -5,6 +5,8 @@ local function bind_keymaps(lsp_name)
     { buffer = 0, desc = string.format('LSP -> %s -> type_definition', lsp_name) })
     vim.keymap.set('n', 'gd', vim.lsp.buf.declaration,
     { buffer = 0, desc = string.format('LSP -> %s -> declaration', lsp_name) })
+    vim.keymap.set('n', 'gD', vim.lsp.buf.definition,
+    { buffer = 0, desc = string.format('LSP -> %s -> definition', lsp_name) })
     vim.keymap.set('n', 'gr', vim.lsp.buf.references,
     { buffer = 0, desc = string.format('LSP -> %s -> references', lsp_name) })
     vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help,
