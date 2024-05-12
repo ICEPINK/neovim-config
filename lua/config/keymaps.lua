8b5b89c4f8e4d312d1c9b47ab46a1bb842438eae
ICEPINK <venayoda@gmail.com>
2024-05-12 21:40:17 +0200

    keymap to quickly jump to run.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 local run_args = ''
-
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Custom -> diagnostic.open_float' })
 vim.keymap.set('n', '<esc>', vim.cmd.nohlsearch, { desc = 'Custom -> nohlsearch' })
 vim.keymap.set('t', '<esc><esc>', '<c-\\><c-n>', { desc = 'Custom -> exit terminal mode' })
 vim.keymap.set('n', '<f5>', function() vim.cmd('!lua .nvim/run.lua ' .. run_args) end, { desc = 'Custom -> nohlsearch' })
 vim.keymap.set('n', '<f6>', function() vim.ui.input({ prompt = 'run.lua args: ', default = run_args }, function(input) run_args = input end) end, { desc = 'Custom -> run args' })
+vim.keymap.set('n', '<f12>', function() vim.cmd.edit('.nvim/run.lua') end, { desc = 'Custom -> open .nvim/run.lua' })
