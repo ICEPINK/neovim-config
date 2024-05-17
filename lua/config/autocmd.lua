77c61fa46283d58abc293efeedaf1f733a405f66
ICEPINK <venayoda@gmail.com>
2024-05-17 12:30:54 +0200

    Commenting for c, c++

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,1 +1,8 @@
-vim.cmd.language("en")
+vim.cmd.language('en')
+vim.api.nvim_create_autocmd({ 'Filetype' }, {
+  desc = 'Commenting for c, c++',
+  pattern = {'cpp', 'hpp'},
+  callback = function(event)
+    vim.bo[event.buf].commentstring = '// %s'
+  end,
+})
