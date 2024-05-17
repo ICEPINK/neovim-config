002a6d9f8fffb495a25f9e073de9ec2d00a62dd7
ICEPINK <venayoda@gmail.com>
2024-05-18 00:23:23 +0200

    neovide settings

---
+++ lua/config/neovide.lua
@@ -0,0 +1,16 @@
+if vim.g.neovide then
+    vim.g.neovide_hide_mouse_when_typing = true
+    vim.g.neovide_fullscreen = true
+    vim.g.neovide_padding_bottom = 0
+    vim.g.neovide_padding_left = 0
+    vim.g.neovide_padding_right = 0
+    vim.g.neovide_padding_top = 0
+    vim.g.neovide_scale_factor = 1.26
+    vim.g.neovide_scroll_animation_length = 0.1
+    vim.g.neovide_text_contrast = 0.8
+    vim.g.neovide_text_gamma = 1.0
+    vim.g.neovide_transparency = 1.0
+    vim.g.neovide_cursor_animation_length = 0.05
+    vim.g.neovide_cursor_trail_size = 0.5
+    vim.o.guifont = "CommitMono Nerd Font:h20:#e-subpixelantialias"
+end
