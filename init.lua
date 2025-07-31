c13891639f90d3bf279fcff9c1252afea2cb644f
ICEPINK <venayoda@gmail.com>
2025-08-01 00:11:14 +0200

    Changed description of <c-f5> keymap

--- init.lua
+++ init.lua
@@ -161,7 +161,7 @@ local run_file = '.nvim/run.lua'
 vim.keymap.set('n', '<esc>', vim.cmd.nohlsearch, { desc = 'Custom -> nohlsearch' })
 vim.keymap.set('n', '<f5>', ':!' .. run_cmd .. ' ' .. run_file .. ' ' .. run_args .. '<CR>', { desc = 'Custom -> run.lua' })
 vim.keymap.set('n', '<s-f5>', function() vim.ui.input({ prompt = 'run args: ', default = run_args }, function(input) run_args = input end) end, { desc = 'Custom -> run args' })
-vim.keymap.set('n', '<c-f5>', function() vim.cmd.edit(run_file) end, { desc = 'Custom -> open .nvim/run.lua' })
+vim.keymap.set('n', '<c-f5>', function() vim.cmd.edit(run_file) end, { desc = 'Custom -> open run_file' })
 vim.keymap.set('n', '<cs-f5>', function() vim.ui.input({ prompt = 'run command: ', default = run_cmd }, function(input) run_cmd = input end) vim.ui.input({ prompt = 'run file: ', default = run_file }, function(input) run_file = input end) end, { desc = 'Custom -> change run command' })
 vim.keymap.set('n', '<leader>i', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end, { desc = 'Custom -> lsp.inlay_hint' })
 vim.keymap.set('n', '<leader>w', function() vim.opt.wrap = not vim.opt.wrap:get() end, { desc = 'Custom -> toggle word wrap' })
